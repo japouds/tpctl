23cd227d3e8a1cd977348c3ab49d893127b962ac
Derrick Burns <derrick@tidepool.org>
2019-12-16 23:15:26 -0800

    Remove routetables

--- cmd/tpctl.sh
+++ cmd/tpctl.sh
@@ -159,11 +159,12 @@ function install_gloo() {
   glooctl uninstall --all
   kubectl delete validatingwebhookconfigurations.admissionregistration.k8s.io gloo-gateway-validation-webhook-gloo-system
   helm delete glooe --purge
-  for x in authconfigs.enterprise proxies settings upstreams upstreamgroups routetables
+  for x in authconfigs.enterprise proxies settings upstreams upstreamgroups 
   do 
 	  kubectl delete crd ${x}.gloo.solo.io
   done
   kubectl delete crd gateways.gateway.solo.io.v2
+  kubectl delete crd routetables.gateway.solo.io
   helm install glooe/gloo-ee --name glooe --namespace gloo-system --set-string license_key=$GLOO_LICENSE_KEY -f $TMP_DIR/gloo-values.yaml
   expect_success "Templating failure gloo/gloo-values.yaml.jsonnet"
 
